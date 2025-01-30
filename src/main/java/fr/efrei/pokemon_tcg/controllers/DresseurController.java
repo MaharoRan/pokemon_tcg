f94bfe0668a51016806cc9dfd9686c309d17a12c
Maharo <mrandriamahefazo@gmail.com>
2025-01-30 18:42:13 +0100

    Implements Entity

--- src/main/java/fr/efrei/pokemon_tcg/controllers/DresseurController.java
+++ src/main/java/fr/efrei/pokemon_tcg/controllers/DresseurController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
